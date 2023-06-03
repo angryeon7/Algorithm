9d01ecbf6741bafcecb150caf7c74085577877e9
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-06-04 04:37:42 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char word[5][15] = {};
+    
+    for(int i = 0; i<5;i++){
+        cin>>word[i];
+    }
+    
+    for(int i = 0; i<15;i++){
+        for(int j = 0; j<5;j++){
+             if (word[j][i] !=NULL) 
+                 cout << word[j][i];
+        }
+       
+    }
+    cout << endl;
+}
