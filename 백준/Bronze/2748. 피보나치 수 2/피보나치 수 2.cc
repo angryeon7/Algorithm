52d519ccc974ec89d378a0c8cca463ba46468c73
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-06-04 05:52:01 +0900

    2748 - 피보나치수2
    2748 - 피보나치수2 주석 추가

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -7,7 +7,7 @@ int main()
     int n;
     
     cin >> n;
-    long long result[91];
+    long long result[91]; // 89번째까지 도달하게 된다면 int형 데이터 타입으로는 범위가 넘어가기 때문에 long long 자료형을 사용
     
     result[0] = 0;
     result[1] = 1;
