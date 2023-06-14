ee348dcb968203ca4ae31b4200a8364d3d5ede24
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-06-14 15:48:31 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    int N;
+    cin >> N;
+    if(N % 2 == 0 ){
+        cout << "CY"<< endl;
+       
+    }else{
+         cout<< "SK"<<endl;
+    }
+}
