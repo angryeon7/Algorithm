828565f6e4520aab25b63b368267427f4b9a9420
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-09-05 15:22:08 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/README.md
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열을 정수로 바꾸기 - 12925 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12925) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.04 ms
+메모리: 16 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
@@ -1,16 +1,3 @@
 func solution(_ s:String) -> Int {
-    
-    var result : Int = 0
-    var text = s[s.startIndex]
-    var num = String(s.dropFirst(1))
-    let n:Int = Int(num) ?? 0
-    
-    if text == "-"{
-         result = n - (n * 2)
-    } else if text == "+"{
-       result = n
-    } else {
-       result = Int(s) ?? 0
-    }
-    return result
+    return Int(s)!
 }
