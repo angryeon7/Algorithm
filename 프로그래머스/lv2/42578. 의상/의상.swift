777977069c18fd127e71dfb30b9e26fb1f378272
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-09-12 20:48:29 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    
+    var category : Set<String> = []
+    var result = 1
+    
+    for i in 0..<clothes.count{
+         category.insert(clothes[i][1])
+    }
+  
+    clothes
+    for j in category{
+        result *= (clothes.filter { $0[1] == j }.count+1)
+    }  
+   
+    return result - 1
+}
